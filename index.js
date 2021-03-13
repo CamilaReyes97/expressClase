7ab9cf5820c3eee77123b062a1a173a34a41eb98
CamilaReyes97 <camila.reyesber97@gmail.com>
2021-03-12 19:10:39 -0500

    Express 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const { connectToDB } = require('./db')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+connectToDB()
+
+require('./routes/course')(app)
+require('./routes/user')(app)
+require('./routes/role')(app)
+
+app.listen(3000, () => {
+    console.log('Nos conectamos')
+})
